fd660cab0ad7f78568033fa0811d1a1eb4201294
Piero Rolando <rolandopiero63@gmail.com>
2024-08-05 11:29:07 -0500

    feat: Add Docker Compose configuration for Java and Prolog services

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,7 @@ import {nextui} from '@nextui-org/theme'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./**/*.{html,js,ts,jsx,tsx,mdx}',",
     "./index.html",
     './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
--- java-prolog-api/src/main/java/com/example/PrologController.java
+++ java-prolog-api/src/main/java/com/example/PrologController.java
@@ -1,15 +1,20 @@
-package com.example.java_prolog_api;
+package com.example;
+
+import org.jpl7.Query;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class PrologController {
 
     @GetMapping("/query")
-    public String queryProlog(@RequestParam String query) {
-        // Aquí se llamará a Prolog
-        return PrologService.queryProlog(query);
+    public String query(@RequestParam String prologQuery) {
+        // This is a simple example. You'd want to add more robust error handling in a real application.
+        Query q = new Query(prologQuery);
+        return q.hasSolution() ? "Yes" : "No";
     }
 }
