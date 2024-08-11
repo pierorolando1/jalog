ca4c25f90a6da442c8ba6ee95dc8560679fdb50a
Piero Rolando <rolandopiero63@gmail.com>
2024-08-11 00:55:16 -0500

    idkwidrn

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,14 @@
 import ProtectedLayout from "@/layouts/protected";
+import { useUser } from "@/stores/stores";
 
 export default function IndexPage() {
+
+  const { user } = useUser()
+
   return (
     <ProtectedLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-
+      <section className="flex flex-col justify-center gap-4 py-8">
+      <h1 className="font-black text-3xl" >Welcome home { user?.name}</h1>
         
 
       </section>
